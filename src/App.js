e49cdf201fa7b748be1e46292b908ac2edbf0d08
jasmin289 <jsmina30@gmail.com>
2023-11-22 15:03:04 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+const changeName =()=>{
+  const name = ["jasmin","dave","miska"]
+  const int = Math.floor(Math.random()*3);
+  return name[int]
+}
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          this is a new line
+          hello {changeName()}!
         </p>
         <a
           className="App-link"
